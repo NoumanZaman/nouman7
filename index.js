cb37107410c1c07859e3175cb19a40ba74c0bd65
NoumanZaman <noumanzamansketches@gmail.com>
2023-12-26 12:57:36 +0000

    adding new files

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+
+
+let name1 = prompt("Enter Your Name")
+let number = prompt("Enter a Number" )
+
+
+if(number>=90 && number<=100){
+    alert(`Hey ${name1} You are pass with A Grade`)
+}
+else if(number>=80 && number<90){
+    alert(`Hey ${name1} You are pass with B Grade`)
+}
+else if(number>=70 && number<80){
+    alert(`Hey ${name1} You are pass with C Grade `)
+}
+else if(number>=60 && number<70){
+    alert(`Hey ${name1} You are pass with D Grade `)
+}
+else if(number>=50 && number<60){
+    alert(`Hey ${name1} You are pass with E Grade `)
+}
+else{
+    alert(`Hey ${name1} you are Fail With F Grade`)
+}
